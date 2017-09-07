e15395b9875acdeffb6bd0670425ca3a9da402df
rohith <rohith.vallabhaneni@ggktech.com>
2017-09-07 18:05:51 +0530

    consumer

---
+++ src/main/java/com/ggktech/rest/client/RestClient.java
@@ -0,0 +1,26 @@
+package com.ggktech.rest.client;
+
+import org.springframework.web.client.RestTemplate;
+
+import com.ggktech.rest.dto.User;
+
+public class RestClient {
+
+	private RestTemplate restTemplate;
+	private String restUrl;
+
+	public RestClient(String restUrl) {
+		this.restUrl = restUrl;
+		restTemplate = new RestTemplate();
+	}
+
+	public User getUser() {
+		User user=null;
+		try {
+			user = restTemplate.getForObject(restUrl + "/user", User.class);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return user;
+	}
+}
---
+++ src/test/java/com/ggktech/rest/client/ConsumerTest.java
@@ -0,0 +1,55 @@
+package com.ggktech.rest.client;
+
+import java.io.IOException;
+
+import org.junit.Assert;
+import org.junit.Rule;
+
+import com.ggktech.rest.dto.User;
+
+import au.com.dius.pact.consumer.ConsumerPactTestMk2;
+import au.com.dius.pact.consumer.MockServer;
+import au.com.dius.pact.consumer.Pact;
+import au.com.dius.pact.consumer.PactProviderRuleMk2;
+import au.com.dius.pact.consumer.dsl.PactDslJsonBody;
+import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
+import au.com.dius.pact.model.RequestResponsePact;
+
+public class ConsumerTest extends ConsumerPactTestMk2{
+
+	 @Rule
+	 public PactProviderRuleMk2 mockProvider = new PactProviderRuleMk2("pact_provider", "localhost", 8080, this);
+	 
+	@Override
+	protected String consumerName() {
+		return "pact_consumer";
+	}
+
+	@Override
+	@Pact(provider="pact_provider", consumer="pact_consumer")
+	protected RequestResponsePact createPact(PactDslWithProvider builder) {
+		 return builder
+		            .given("test state") // NOTE: Using provider states are optional, you can leave it out
+		            .uponReceiving("ExampleJavaConsumerPactTest test interaction")
+		                .path("/user")
+		                .method("GET")
+		            .willRespondWith()
+		                .status(200)
+		                .body(new PactDslJsonBody().integerType("id", 1).stringValue("name", "John"))
+		            .toPact();
+	}
+
+	@Override
+	protected String providerName() {
+		return "pact_provider";
+	}
+
+	@Override
+	protected void runTest(MockServer server) throws IOException {
+		RestClient restClient = new RestClient(server.getUrl());
+		User user = restClient.getUser();
+		Assert.assertNotNull(user);
+	}
+
+	
+}
