8879c4463a64fe2f8a140e396b1fc44a388ea469
rohith <rohith.vallabhaneni@ggktech.com>
2017-09-07 18:32:05 +0530

    consumer modified

--- src/main/java/com/ggktech/rest/client/RestClient.java
+++ src/main/java/com/ggktech/rest/client/RestClient.java
@@ -1,9 +1,11 @@
 package com.ggktech.rest.client;
 
+import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 import com.ggktech.rest.dto.User;
 
+@Component
 public class RestClient {
 
 	private RestTemplate restTemplate;
